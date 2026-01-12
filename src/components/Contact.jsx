c6e50e2d0ba00ca4b47ddb5a94784230dc67c085
hosannadevarakonda9-lang <hosannadevarakonda9@gmail.com>
2026-01-12 16:40:36 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,7 +10,7 @@ function Contact() {
         <p>📱 +91 7569954352</p>
 
         <div className="social-links">
-          <a href="https://hosanna-portfolio.netlify.app/" target="_blank">GitHub</a>
+          <a href="https://github.com/hosannadevarakonda9-lang/" target="_blank">GitHub</a>
           <a href="https://www.linkedin.com/in/devarakonda-hosanna-3b915b367?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank">LinkedIn</a>
         </div>
       </div>
